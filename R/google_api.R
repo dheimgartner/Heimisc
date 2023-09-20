a8e369526be10ec54b41d6445e118dc486f8e4ed
dheimgartner <d.heimgartners@gmail.com>
2023-09-20 13:01:14 +0200

    only return response -> extraction of relevant fields left to user

--- R/google_api.R
+++ R/google_api.R
@@ -1,6 +1,5 @@
 #' @export
 google_geocode <- function(street, street_nr, zip, country, api_key = NULL) {
-    out <- list()
     if (is.null(api_key)) {
         api_key <- Sys.getenv("GOOGLE_API_KEY")
         assertthat::assert_that(stringr::str_count(api_key) > 0,
@@ -15,23 +14,11 @@ google_geocode <- function(street, street_nr, zip, country, api_key = NULL) {
     )
     response <- httr::GET(api_url)
 
-    if (http_status(response)$category == "Success") {
-        location <- content(response)$results[[1]]$geometry$location
-        out$response <- response
-        out$lat <- location$lat
-        out$lon <- location$lng
-    } else {
-        out$response
-        out$lat <- NA_real_
-        out$lon <- NA_real_
-    }
-
-    return(out)
+    return(response)
 }
 
 #' @export
 google_reverse_geocode <- function(lon, lat, api_key = NULL) {
-    out <- list()
     if (is.null(api_key)) {
         api_key <- Sys.getenv("GOOGLE_API_KEY")
         assertthat::assert_that(stringr::str_count(api_key) > 0,
@@ -43,14 +30,5 @@ google_reverse_geocode <- function(lon, lat, api_key = NULL) {
     api_url <- sprintf("%s?%s", base_url, utils::URLencode(query))
     response <<- httr::GET(api_url)
 
-    if (httr::http_status(response)$category == "Success") {
-        zip <- content(response)$results[[1]]$address_components[[7]]$short_name
-        out$response <- response
-        out$zip <- as.numeric(zip)
-    } else {
-        out$response <- response
-        out$zip <- NA_real_
-    }
-
-    return(out)
+    return(response)
 }
