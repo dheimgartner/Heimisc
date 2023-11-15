7074db8e6b1ca85d87f5aad596b58e1b56ca8414
dheimgartner <d.heimgartners@gmail.com>
2023-11-15 22:57:29 +0100

    Namespace export

--- R/cran.R
+++ R/cran.R
@@ -1,12 +1,15 @@
+#' @export
 get_cran <- function(local_cran = "_CRAN") {
   home <- Sys.getenv("HOME")
   return(paste("file:", home, local_cran, sep = "/"))
 }
 
+#' @export
 install <- function(pkg, ...) {
   install.packages(pkg, repos = get_cran(), ...)
 }
 
+#' @export
 uninstall <- function(pkg, ...) {
   remove.packages(pkg, ...)
 }
